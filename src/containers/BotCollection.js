0589398de88c5f3fbef43e05aa29a759d1a35ec5
ValentinaPanic <valentina.panic87@gmail.com>
2021-02-05 15:45:08 -0700

    God help

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
 
 class BotCollection extends Component {
-  //your code here
 
+  constructor(props){
+    super()
+     this.state= {
+       bots: []
+     }
+  }
+
+  componentDidMount(){
+    
+    const BASE_URL = "http://localhost:6001/bots"
+      fetch(BASE_URL)
+      .then(response => response.json())
+      .then(bots => {
+        // console.log(bots)
+        this.setState({
+          bots: bots
+          
+        })
+      })
+    
+  }
+  
   render() {
+    const { bots } = this.state
+    // console.log(bots)
     return (
       <div className="ui four column grid">
         <div className="row">
-          {/*...and here..*/}
+          {bots.map(bot =>{
+            console.log(bot.name)
+          })}
           Collection of all bots
         </div>
       </div>
